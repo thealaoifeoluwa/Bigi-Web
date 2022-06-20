295012a00d251f71c1defba0b52b956066d7efc3
Alao Ifeoluwa <alaoifeoluwa@gmail.com>
2022-06-20 18:28:19 +0100

    hamburger solved

--- js/main.js
+++ js/main.js
@@ -1,13 +1,34 @@
-// Scroll iocn fade ---------------------------------------
-$('body').scroll(function(){
-    $('#menu').fadeOut();
+// // Scroll iocn fade ---------------------------------------
+// $('body').scroll(function(){
+//     $('#menu').fadeOut();
 
-    var scrollA = $('body').scrollTop();
+//     var scrollA = $('body').scrollTop();
 
-    setTimeout(function(){
-        if(scrollA == $('body').scrollTop()){
-            $('#menu').fadeIn();
-        }
-    }, 200);
-})
-// ==========================================================
+//     setTimeout(function(){
+//         if(scrollA == $('body').scrollTop()){
+//             $('#menu').fadeIn();
+//         }
+//     }, 200);
+// })
+// // ==========================================================
+
+const hamburger = document.querySelector(".hamburger");
+const headerleft = document.querySelector(".header-left");
+const body = document.querySelector("body");
+
+
+hamburger.addEventListener("click",() => {
+    hamburger.classList.toggle("active");
+    headerleft.classList.toggle("active");
+    body.classList.toggle("active");
+
+    
+}) 
+
+// document.querySelectorAll(".menu-link").forEach(n => n.
+// addEventListener("click",() => {
+//     hamburger.classList.remove("active");
+//     headerleft.classList.remove("active");
+//     body.classList.remove("active");
+
+// }))
