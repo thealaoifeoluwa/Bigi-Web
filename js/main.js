7eb0ada2df377a1e3dedf811b042eeb31dd7872a
Alao Ifeoluwa <alaoifeoluwa@gmail.com>
2022-06-20 23:57:29 +0100

    hamburger bg color change

--- js/main.js
+++ js/main.js
@@ -14,21 +14,12 @@
 
 const hamburger = document.querySelector(".hamburger");
 const headerleft = document.querySelector(".header-left");
-const body = document.querySelector("body");
+
 
 
 hamburger.addEventListener("click",() => {
     hamburger.classList.toggle("active");
     headerleft.classList.toggle("active");
-    body.classList.toggle("active");
 
     
 }) 
-
-// document.querySelectorAll(".menu-link").forEach(n => n.
-// addEventListener("click",() => {
-//     hamburger.classList.remove("active");
-//     headerleft.classList.remove("active");
-//     body.classList.remove("active");
-
-// }))
